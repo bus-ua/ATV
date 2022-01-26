17aab831bc3533b7181bf912fb0f463ef76e5381
bus-ua <96583481+bus-ua@users.noreply.github.com>
2022-01-26 17:01:21 +0200

    Add files via upload

---
+++ js/slick.js
@@ -0,0 +1,37 @@
+// $(document).ready(function(){
+//     $('.slider').slick({
+//         speed: 500,
+//         slidesToShow: 3,
+//         centerMode: true,
+//         slidesToScroll: 3,
+//         responsive: [
+//           {
+//             breakpoint: 1124,
+//             settings: {
+//               slidesToShow: 2,
+//               slidesToScroll: 2,
+//               infinite: true,
+//             }
+//           },
+//           {
+//             breakpoint: 600,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1
+//             }
+//           },
+//           {
+//             breakpoint: 480,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1,
+//                 centerMode: false,
+
+//             }
+//           }
+//           // You can unslick at a given breakpoint now by adding:
+//           // settings: "unslick"
+//           // instead of a settings object
+//         ]
+//     });
+// });
